758d1d2d8691714dce14b49fa9a4cb49ed64f147
DrMahmoudCodes <mahmoudhanwer@gmail.com>
2026-01-01 15:39:35 +0200

    1

---
+++ story_content/triggers.js
@@ -0,0 +1,34 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5st7CCXZ2bD":
+        Script1();
+        break;
+      case "6pGB0o2YKlQ":
+        Script2();
+        break;
+      case "5yaHqCfTuyb":
+        Script3();
+        break;
+      case "68ImUeX21lg":
+        Script4();
+        break;
+      case "6WPjWu0K51O":
+        Script5();
+        break;
+      case "6gK65YtCmMu":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
